3b49d2578b9185d326fc0c8dbc957ac20a880914
Marcelo McAndrew <marcelomcandrew@gmail.com>
2018-10-26 10:49:38 -0400

    starting services works on windows

--- guardian/guardian.go
+++ guardian/guardian.go
@@ -1,11 +1,11 @@
 package guardian
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
 	"net/http"
 	"os/exec"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -143,6 +143,7 @@ func (gg *GladiusGuardian) StopService(name string) error {
 	return gg.stopServiceInternal(name)
 }
 
+// StartService - Start the controld and networkd
 func (gg *GladiusGuardian) StartService(name string, env []string) error {
 	if name == "all" || name == "" {
 		var result *multierror.Error
@@ -217,6 +218,7 @@ func (gg *GladiusGuardian) stopServiceInternal(name string) error {
 	return nil
 }
 
+// AddLogClient - Add logging client
 func (gg *GladiusGuardian) AddLogClient(serviceName string, w http.ResponseWriter, r *http.Request) {
 	gg.mux.Lock()
 	defer gg.mux.Unlock()
@@ -230,6 +232,7 @@ func (gg *GladiusGuardian) AddLogClient(serviceName string, w http.ResponseWrite
 	gg.serviceWebSockets[serviceName] = append(gg.serviceWebSockets[serviceName], conn)
 }
 
+// AppendToLog - Add string to the service logs
 func (gg *GladiusGuardian) AppendToLog(serviceName, line string) {
 	if gg.serviceLogs[serviceName] == nil {
 		gg.serviceLogs[serviceName] = NewFixedSizeLog(viper.GetInt("MaxLogLines"))
@@ -246,69 +249,77 @@ func (gg *GladiusGuardian) checkTimeout() error {
 }
 
 func (gg *GladiusGuardian) spawnProcess(name, location string, env []string, timeout *time.Duration) (*exec.Cmd, error) {
+	// unix
 	p := exec.Command(location)
-	p.Env = env
+
+	//specifically for windows
+	if runtime.GOOS == "windows" {
+		// location = `C:\Program Files (x86)\Gladius Node\` + location + ".exe"
+		p = exec.Command("cmd.exe", "/C", "start", location)
+	}
+	// p.Env = env
 
 	// Create standard err and out pipes
-	stdOut, err := p.StdoutPipe()
-	if err != nil {
-		return nil, fmt.Errorf("Error creating StdoutPipe for command: %s", err)
-	}
-	stdErr, err := p.StderrPipe()
-	if err != nil {
-		return nil, fmt.Errorf("Error creating StderrPipe for command: %s", err)
-	}
+	// stdOut, err := p.StdoutPipe()
+	// if err != nil {
+	// 	return nil, fmt.Errorf("Error creating StdoutPipe for command: %s", err)
+	// }
+	// stdErr, err := p.StderrPipe()
+	// if err != nil {
+	// 	return nil, fmt.Errorf("Error creating StderrPipe for command: %s", err)
+	// }
 
-	// Read both of those in
-	scanner := bufio.NewScanner(stdOut)
-	stdErrScanner := bufio.NewScanner(stdErr)
-	go func() {
-		defer stdOut.Close()
-		for scanner.Scan() {
-			gg.AppendToLog(name, scanner.Text())
-		}
-	}()
-	go func() {
-		defer stdErr.Close()
-		for stdErrScanner.Scan() {
-			gg.AppendToLog(name, stdErrScanner.Text())
-		}
-	}()
+	// Pipe stdout to the logs
+	// scanner := bufio.NewScanner(stdOut)
+	// stdErrScanner := bufio.NewScanner(stdErr)
+	// go func() {
+	// 	defer stdOut.Close()
+	// 	for scanner.Scan() {
+	// 		gg.AppendToLog(name, scanner.Text())
+	// 	}
+	// }()
+	// go func() {
+	// 	defer stdErr.Close()
+	// 	for stdErrScanner.Scan() {
+	// 		gg.AppendToLog(name, stdErrScanner.Text())
+	// 	}
+	// }()
 
 	// Start the command
-	err = p.Start()
+	err := p.Start()
+	fmt.Printf("Starting process: %s\n", location)
 	if err != nil {
 		log.WithFields(log.Fields{
 			"exec_location":    location,
 			"environment_vars": strings.Join(env, ", "),
 			"err":              err,
 		}).Warn("Couldn't spawn process")
-		return nil, fmt.Errorf("Error starting process: %s", err)
+		return nil, fmt.Errorf("\nError starting process: %s", err)
 	}
 
-	go func() {
-		err := p.Wait()
-		gg.services[name] = nil // Set out service to nil when it dies
-		if err != nil {
-			// Only log errors if we didn't kill it
-			if err.Error() != "signal: killed" {
-				log.WithFields(log.Fields{
-					"exec_location":    location,
-					"environment_vars": strings.Join(env, ", "),
-					"err":              err,
-				}).Error("Service errored out")
-				gg.AppendToLog(name, "Exiting... "+err.Error())
-			}
-		}
-	}()
+	// go func() {
+	// 	err := p.Wait()
+	// 	gg.services[name] = nil // Set out service to nil when it dies
+	// 	if err != nil {
+	// 		// Only log errors if we didn't kill it
+	// 		if err.Error() != "signal: killed" {
+	// 			log.WithFields(log.Fields{
+	// 				"exec_location":    location,
+	// 				"environment_vars": strings.Join(env, ", "),
+	// 				"err":              err,
+	// 			}).Error("Service errored out")
+	// 			gg.AppendToLog(name, "Exiting... "+err.Error())
+	// 		}
+	// 	}
+	// }()
 
 	// Wait for the process to start
-	time.Sleep(*timeout)
-	if p.ProcessState != nil { // ProcessState is only non-nil if p.Wait() concludes
-		if p.ProcessState.Exited() {
-			return nil, fmt.Errorf("process %s already exited, check the logs for errors", name)
-		}
-	}
+	// time.Sleep(*timeout)
+	// if p.ProcessState != nil { // ProcessState is only non-nil if p.Wait() concludes
+	// 	if p.ProcessState.Exited() {
+	// 		return nil, fmt.Errorf("process %s already exited, check the logs for errors", name)
+	// 	}
+	// }
 	return p, nil
 
 }
---
+++ win/win.go
@@ -0,0 +1,91 @@
+package win
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+// TH32CS_SNAPPROCESS is described in https://msdn.microsoft.com/de-de/library/windows/desktop/ms682489(v=vs.85).aspx
+const TH32CS_SNAPPROCESS = 0x00000002
+
+// WindowsProcess is an implementation of Process for Windows.
+type WindowsProcess struct {
+	ProcessID       int
+	ParentProcessID int
+	Exe             string
+}
+
+func Processes() ([]WindowsProcess, error) {
+	handle, err := windows.CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
+	if err != nil {
+		return nil, err
+	}
+	defer windows.CloseHandle(handle)
+
+	var entry windows.ProcessEntry32
+	entry.Size = uint32(unsafe.Sizeof(entry))
+	// get the first process
+	err = windows.Process32First(handle, &entry)
+	if err != nil {
+		return nil, err
+	}
+
+	results := make([]WindowsProcess, 0, 50)
+	for {
+		results = append(results, NewWindowsProcess(&entry))
+
+		err = windows.Process32Next(handle, &entry)
+		if err != nil {
+			// windows sends ERROR_NO_MORE_FILES on last process
+			if err == syscall.ERROR_NO_MORE_FILES {
+				return results, nil
+			}
+			return nil, err
+		}
+	}
+}
+
+func FindProcessByName(processes []WindowsProcess, name string) *WindowsProcess {
+	for _, p := range processes {
+		if strings.ToLower(p.Exe) == strings.ToLower(name) {
+			return &p
+		}
+	}
+	return nil
+}
+
+func NewWindowsProcess(e *windows.ProcessEntry32) WindowsProcess {
+	// Find when the string ends for decoding
+	end := 0
+	for {
+		if e.ExeFile[end] == 0 {
+			break
+		}
+		end++
+	}
+
+	return WindowsProcess{
+		ProcessID:       int(e.ProcessID),
+		ParentProcessID: int(e.ParentProcessID),
+		Exe:             syscall.UTF16ToString(e.ExeFile[:end]),
+	}
+}
+
+func GetProcessWindows(name string) (int, error) {
+	procs, err := Processes()
+	if err != nil {
+		log.Fatal(err)
+	}
+	explorer := FindProcessByName(procs, name)
+	if explorer != nil {
+		// found it
+		return explorer.ProcessID, nil
+	}
+
+	return 0, fmt.Errorf("No process called: %s", name)
+}
